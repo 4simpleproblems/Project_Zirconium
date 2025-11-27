98c929b27ff63d3659e124ca34eb54e4e966bedf
4SP <4simpleproblems@gmail.com>
2025-11-27 05:03:30 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -34,19 +34,7 @@
  * 26. **(UPDATED)** THEME LOGIC: Removed hardcoded theme name checks from `window.applyTheme`. Theme properties (like logo and colors) are now pulled *only* from the theme object, falling back to `DEFAULT_THEME`.
  */
 
-// =========================================================================
-// >> ACTION REQUIRED: PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE <<
-// =========================================================================
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAZBKAckVa4IMvJGjcyndZx6Y1XD52lgro",
-    authDomain: "project-zirconium.firebaseapp.com",
-    projectId: "project-zirconium",
-    storageBucket: "project-zirconium.firebaseapp.com",
-    messagingSenderId: "1096564243475",
-    appId: "1:1096564243475:web:6d0956a70125eeea1ad3e6",
-    measurementId: "G-1D4F692C1Q"
-};
-// =========================================================================
+
 
 // --- Configuration ---
 const PAGE_CONFIG_URL = '../page-identification.json';
