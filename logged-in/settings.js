81304a36eb880d19405cfc627e89190e7f1c99a5
v5-4simpleproblems <4simpleproblems@gmail.com>
2025-11-29 00:42:56 -0500

    Add new hat options to Mibi Avatar Creator

--- logged-in/settings.js
+++ logged-in/settings.js
@@ -1283,7 +1283,7 @@
         const MIBI_ASSETS = {
             eyes: ['default-eyes.png', 'glasses.png', 'odd.png'],
             mouths: ['default-mouth.png', 'drool.png', 'meh.png', 'no-clue.png', 'sad.png', 'wow.png'],
-            hats: ['strawhat.png', 'tophat.png'],
+            hats: ['strawhat.png', 'tophat.png', 'partyhat.png', 'halo.png', 'toiletpaper.png'],
             colors: [
                 '#EF4444', '#F97316', '#F59E0B', '#22C55E', '#3B82F6', 
                 '#6366F1', '#A855F7', '#EC4899', '#000000', '#FFFFFF'
