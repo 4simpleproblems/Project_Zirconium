9fc2d0861c663161dcb182324f5d85472dac00ca
v5-4simpleproblems <4simpleproblems@gmail.com>
2025-11-29 21:36:39 -0500

    feat: Update Storage rules to allow read access to Soundboard assets

--- storage_rules.js
+++ storage_rules.js
@@ -1,8 +1,14 @@
 rules_version = '2';
 service firebase.storage {
   match /b/{bucket}/o {
+    // Deny all reads and writes by default
     match /{allPaths=**} {
-      allow read, write: if false; // Deny all reads and writes by default
+      allow read, write: if false;
+    }
+
+    // Allow authenticated users to read soundboard files
+    match /Soundboard/{soundboardPath=**} {
+      allow read: if request.auth != null;
     }
   }
 }
